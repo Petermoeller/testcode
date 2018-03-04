3b134fc0dedbfefd22949076836ac24f0f2ba5f1
Peter Moeller <petermoeller@me.com>
2018-03-04 14:09:31 +0100

    added comments to the main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
+/*
+A code to mess around with to get to know gitHub
+ */
 #include "Arduino.h"
 
 int test = 0;
 
 void setup(){
 
-
 }
 
 void loop () {
-for (size_t i = 0; i < 10; i++) {
- test = test ++;
-  if (test == 1000) {
-    test = 0;
-  }
-  delay(1000);
-}
+        for (int i = 0; i < 10; i++) {
+                test = test++;
+                if (test == 1000) {
+                        test = 0;
+                }
+                delay(1000);
 
-
+                //this is the main function
+        }
 }
